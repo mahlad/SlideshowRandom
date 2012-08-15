5afbe74050134b05d8f083fcde3d3057815e60c8
mahlad <mahlad365@gmail.com>
2012-08-15 15:06:46 +0430

    1-first we shoulde set the random function:    -We need a function with entire variable for get random number in return of that. like(Math.rnd)             - save the result of main Math.random in a global variable    -We can use $(function) insted of $(document).ready(selector) According to:      https://github.com/mahlad/SlideshowRandom/issues/1    - At final set the animation and css we like for our slideshow

--- js/SlideTow.js
+++ js/SlideTow.js
@@ -1,23 +1,33 @@
-$(document).ready(function() {
-	var a=Math.random()*(90-40)+40;
-	var b=Math.random()*(70-40)+40;
-	var c=Math.floor(Math.random()*(360-90)+90);
+Math.rnd = Math.random;
+Math.random=function(a,b){
+	if(!a && !b) return Math.rnd();
+	return Math.floor(Math.rnd()*(b-a+1))+a;
+}
+$(function(){
+	var pics = $('div.slTow a');
 
-	$('div.slTow  a').animate({
-		"top":b+'px',
-		"left":a+'px',
-		'-moz-transform':'rotate('+c+'deg)'
-		
+	pics.each(function(){
+	/*$('tagName').each(function(){});:baraye har yek az taghaye
+	'tagName' yekbar  tabe dakhele each() ra ejra mikonad*/ 
+		$(this).animate({
+			left:Math.random(50,500),
+			top:Math.random(50,300),
+			opacity:'0.9',
+			'-moz-transform':'rotate('+Math.random(-90,90)+'deg)'
+			
+		},500);
 	});
-	/*$('div.slTow  a').mouseover(function(){
+	pics.mouseover(function(){
 		$(this).css({
-			'z-index':'2';
+			'z-index':2,
+			opacity:'0.98'
 		});
 	});
-	$('div.slTow  a').mouseout(function(){
+	pics.mouseout(function(){
 		$(this).css({
-			'z-index':'0';
+			'z-index':1,
+			opacity:'0.9'
 		});
 	});
-	*/
+
 });
